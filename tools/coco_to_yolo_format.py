215c85828a8cb6159b329b9f96141dc3c8870d1c
serena-mou <serena.ya.mou@gmail.com>
2024-10-16 16:03:20 +1000

    separate merge functionality from coco convert

--- tools/coco_to_yolo_format.py
+++ tools/coco_to_yolo_format.py
@@ -20,7 +20,7 @@ import argparse
 
 
 class COCO2YOLOBB():
-    def __init__(self, json_file, save_location, merger, merge_file, new_cls):
+    def __init__(self, json_file, save_location):
 
         # load in the json file
         #json_file = "/home/serena/Data/SCTLD/RAW/1_100/annotations/instances_default.json" 
@@ -29,9 +29,6 @@ class COCO2YOLOBB():
         #self.data = json.load(f)
         self.in_files = json_file #"/home/serena/Data/SCTLD/RAW/"
         self.save_location = save_location #"/home/serena/Data/SCTLD/Processed/"
-        self.merger = merger
-        self.merge_file = merge_file
-        self.new_cls = new_cls
 
 
     def get_info(self, data):
@@ -99,26 +96,6 @@ class COCO2YOLOBB():
         with open(test_yaml_path, 'w') as outfile:
             yaml.dump(test, outfile, sort_keys=False)
   
-    def write_class_merger(self, classes):
-        # generate yaml file with new classes and old classes
-        # for the purpose of merging classes 
-        yaml_path = os.path.join(self.save_location,"class_merger.yaml")
-
-        # dictionary of {0: class0, 1: class1...}
-        cls_dict = {k:v for k,v in enumerate(classes)}
-        new_cls = self.new_cls.split(',')
-        
-        new_cls_dict =  {k:v for k,v in enumerate(new_cls)}
-
-        data = {
-            "new_classes": new_cls_dict,
-            "old_classes": cls_dict
-        }
-
-        with open(yaml_path, 'w') as outfile:
-            yaml.dump(data, outfile, sort_keys=False)
-
-    
     def write_label_summary(self, summary_dict):
         csv_path = os.path.join(self.save_location,"label_summary.csv")
 
@@ -127,34 +104,6 @@ class COCO2YOLOBB():
             writer = csv.writer(outfile)
             for key, value in summary_dict.items():
                 writer.writerow([key,value])
-
-
-    def read_class_merger(self, merger_yaml):
-        # reading yaml file with ,new_cls_idx on end of each line 
-        # return dictionary of mappings to new classes
-
-
-        new_class_mapping = {}
-        with open(merger_yaml, 'r') as stream:
-            data_loaded = yaml.safe_load(stream)
-
-        to_merge = data_loaded['old_classes']
-        new_classes = data_loaded['new_classes']
-        
-        self.new_cls = []
-        for cls in new_classes:
-            self.new_cls.append(new_classes[cls])
-        
-        for cls in to_merge:
-            if ',' not in str(to_merge[cls]):
-                sys.exit("class merger yaml not updated to correct format")
-            [n, merge] = str(to_merge[cls]).split(',')
-            if int(merge) not in new_class_mapping:
-                new_class_mapping[int(merge)] = [cls]
-            else:
-                new_class_mapping[int(merge)].append(cls)
-
-        return new_class_mapping
 
 
     def bbx_converter(self, bbx_raw, im_sz):
@@ -169,7 +118,7 @@ class COCO2YOLOBB():
 
         return [xn, yn, wn, hn]
 
-    def write_txt(self, classes, img_names, cls, img_ids, bbxs, im_sz, mapping):
+    def write_txt(self, classes, img_names, cls, img_ids, bbxs, im_sz):
         
         # location to save labels
         out_folder = os.path.join(self.save_location,"all_labels")
@@ -204,18 +153,9 @@ class COCO2YOLOBB():
             for idx in all_im_idx:
                 # print(idx, cls, mapping)
                 # if classes are being remapped then use the mapping (dict) to find new class
-                if mapping is not None:
-                    self.write_yaml(self.new_cls)
-                    for new_cls in mapping:
-                        if cls[idx] in mapping[new_cls]:
-                            idx_class = new_cls
-                        else:
-                            continue# print("Remapping of classes error")
-
-                else: 
-                    idx_class = cls[idx] 
-                    #print(cls,idx)
-                    self.write_yaml(classes)
+                idx_class = cls[idx] 
+                #print(cls,idx)
+                self.write_yaml(classes)
                 [xn, yn, wn, hn] = self.bbx_converter(bbxs[idx], im_sz[idx])
                 lines.append((idx_class, xn, yn, wn, hn))
 
@@ -272,19 +212,8 @@ class COCO2YOLOBB():
             # print(img_names)
             # input()
             ## For the first run, generate the class merger file            
-            if self.merger:
-                if self.merge_file:
-                    mapping = self.read_class_merger(self.merge_file)
 
-                elif self.merge_file == None:
-                    self.write_class_merger(classes)
-                    print("\n**NOTE** An assumption is being made that if multiple jsons are being loaded, all classes exist in first json to be loaded\n")
-                    sys.exit("Class merger file written to %s, please update and load on next run. Exiting... "%(os.path.join(self.save_location,"class_merger.yaml")))
-
-            else:
-                mapping = None    
-            
-            self.write_txt(classes, img_names, cls, img_ids, bbxs, im_sz, mapping)
+            self.write_txt(classes, img_names, cls, img_ids, bbxs, im_sz)
 
             # self.write_txt(img_names, cls, img_ids, bbxs, im_sz, mapping)
             
@@ -310,12 +239,6 @@ def arg_parse():
     parser.add_argument("--save", dest = "save_location",
             help = "Path to save labels", default = None, type = str, required=True)
     
-    parser.add_argument("--merge", dest = "merge_file",
-            help = "Path to merge yaml file", default = None, type = str, required=False)
-    
-    parser.add_argument("--newclasses", dest = "new_cls",
-            help = "The class names to be merged to, separated by commas. Ex: cls0,cls1,cls2", default = None, type = str, required=False)
-
     return parser.parse_args()
 
 
@@ -324,41 +247,7 @@ def main():
 
     #json_file = input("Path to JSON file or regex to files: ")
     #save_location = input("Path to save labels: ")
-    merge = input("Do you need to merge classes? Y/N: ")
-    if merge == "Y":
-        merger = True
-
-        ex_merge_file = input("\nDo you want to use an existing merge file? Y/N: ")
-        if ex_merge_file == "Y":
-            if args.merge_file == None:
-                sys.exit("Please add merge file path argument to --merge")
-            
-            merge_file = args.merge_file#input("Path to merge yaml: ")
-            if args.new_cls:
-                print("--newclasses being ignored\n")
-            new_cls = None
-        elif ex_merge_file == "N":
-            if args.merge_file:
-                print("--merge being ignored\n")
-            merge_file = None
-            if args.new_cls == None:
-                sys.exit("Please add new classes argument to --newclasses")
-            new_cls = args.new_cls#input("What are the new classes to merge to? cls1,cls2,...,clsn : ")
-        else:
-            raise ValueError("Please type Y or N and enter")
-
-    elif merge == "N":
-        if args.merge_file:
-            print("--merge being ignored\n")
-        if args.new_cls:
-            print("--newclasses being ignored\n")
-        merger = False
-        merge_file = None
-        new_cls = None
-    else:
-        raise ValueError("Please type Y or N and enter")
-
-    test = COCO2YOLOBB(args.json_file, args.save_location, merger, merge_file, new_cls)
+    test = COCO2YOLOBB(args.json_file, args.save_location)
     test.run()
     print("DONE")
 
---
+++ tools/merge_classes.py
@@ -0,0 +1,231 @@
+#/usr/bin/env python3
+
+"""
+Author: Serena Mou
+Created:  16 October 2024
+
+===
+Merges classes with a class_merger.yaml for labels that are in YOLO format
+===
+
+"""
+
+import os
+import yaml
+import glob
+import sys
+import argparse
+
+class mergeClasses():
+    def __init__(self, labels_in, save, merge_file, data_file, new_cls, use_case):
+
+        self.labels_in = labels_in
+        self.save = save
+        self.merge_file = merge_file
+        self.data_file = data_file
+        # extract classes from data.yaml
+        if self.data_file is not None:
+            with open(self.data_file,'r') as stream:
+                data_loaded = yaml.safe_load(stream)
+
+            self.old_cls_dict = data_loaded["names"]
+
+        if new_cls is not None:
+            new_cls = new_cls.split(',')
+            self.new_cls_dict = {k:v for k,v in enumerate(new_cls)}
+        
+        self.use_case = use_case
+
+    def write_class_merger(self):
+        print("Generating a class_merger.yaml file with data from %s and the new classes, [%s]\n"%(self.data_file, self.new_cls))
+        # generate yaml file with new classes and old classes
+        # for the purpose of merging classes 
+        yaml_path = os.path.join(self.save,"class_merger.yaml")
+
+
+
+        # dictionary of {0: class0, 1: class1...}
+        cls_dict = self.old_cls_dict
+        #cls_dict = {k:v for k,v in enumerate(classes)}
+        new_cls_dict = self.new_cls_dict
+
+        data = {
+            "new_classes": new_cls_dict,
+            "old_classes": cls_dict
+        }
+        print("Saving %s"%yaml_path)
+
+        with open(yaml_path, 'w') as outfile:
+            yaml.dump(data, outfile, sort_keys=False)
+
+    def read_class_merger(self):
+        # reading yaml file with ,new_cls_idx on end of each line 
+        # return dictionary of mappings to new classes
+
+        new_class_mapping = {}
+        with open(self.merge_file, 'r') as stream:
+            data_loaded = yaml.safe_load(stream)
+
+        to_merge = data_loaded['old_classes']
+        new_classes = data_loaded['new_classes']
+        
+        self.new_cls = []
+        for cls in new_classes:
+            self.new_cls.append(new_classes[cls])
+        
+        for cls in to_merge:
+            if ',' not in str(to_merge[cls]):
+                sys.exit("class_merger yaml not updated to correct format")
+            [n, merge] = str(to_merge[cls]).split(',')
+            if int(merge) not in new_class_mapping:
+                new_class_mapping[int(merge)] = [cls]
+            else:
+                new_class_mapping[int(merge)].append(cls)
+
+        return new_class_mapping
+    
+    def change_classes(self, mapping):
+        
+        # location to save labels
+        out_folder = os.path.join(self.save,"all_labels")
+
+        if not os.path.isdir(out_folder):
+            os.mkdir(out_folder)
+
+        # add all labels to list
+        labels_in = glob.glob(os.path.join(self.labels_in,"*.txt"))
+
+        # for each label, open file, load each line, change first value based on mapping
+        for label in labels_in:
+            # grab just the file name
+            save_name = label.split('/')[-1]
+            
+            new_lines = []
+            f = open(label, "r")
+            for line in f:
+                # get first val
+                first_space = line.find(" ")
+                old_cls = int(line[0:first_space])
+                new_cls = None
+                for map_new in mapping:
+                    if old_cls in mapping[map_new]:
+                        new_cls = map_new
+                    else:
+                        continue
+                
+                if new_cls == None:
+                    print(old_cls)
+                    sys.exit("ERROR: Class merge error. Old class not found in %s"%label)
+                new_lines.append("%s %s"%(str(new_cls),line[first_space+1:]))
+
+            save_out = os.path.join(out_folder,save_name)
+            with open(save_out, "w") as f:
+                for line in new_lines:
+                    f.write(line)
+
+
+    def write_yaml(self):
+        # generate yaml file with each class name
+        yaml_path = os.path.join(self.save,"data.yaml")
+        test_yaml_path = os.path.join(self.save,"test.yaml")
+        # dictionary of {0: class0, 1: class1...}
+        new_cls_enum = {k:v for k,v in enumerate(self.new_cls)}
+        data = {
+            "path":self.save,
+            "train": "train",
+            "val": "valid",
+            "names": new_cls_enum
+        }
+
+        test = {
+            "path":self.save,
+            "train": "train",
+            "val": "test",
+            "names": new_cls_enum
+        }
+
+
+        with open(yaml_path, 'w') as outfile:
+            yaml.dump(data, outfile, sort_keys=False)
+
+        with open(test_yaml_path, 'w') as outfile:
+            yaml.dump(test, outfile, sort_keys=False) 
+
+    def run(self):
+
+        # load new classes and data.yaml, write a fresh class_merger.yaml
+        if self.use_case.lower() == "g":
+            self.write_class_merger()
+            sys.exit()
+
+        # load class_merger.yaml to genereate mapping, open, edit, save all labels
+        if self.use_case.lower() == "m":
+            mapping = self.read_class_merger()
+            self.change_classes(mapping)
+            self.write_yaml()
+
+def arg_parse():
+    parser = argparse.ArgumentParser(
+        description='Generate a class_merger.yaml or merge classes with a class_merger.yaml for labels that are in YOLO format')
+
+    parser.add_argument("--newclasses", dest = "new_cls",
+            help = "The class names to be merged to, separated by commas. Ex: cls0,cls1,cls2", 
+            default = None, type = str, required=False)
+    
+    parser.add_argument("--data", dest = "data",
+            help = "Path to data.yaml containing all original classes", 
+            default = None, type = str, required=False)
+    
+    parser.add_argument("--labels_in", dest = "labels_in",
+            help = "Path to labels to be merged", 
+            default = None, type = str, required=False)
+    
+    parser.add_argument("--save", dest = "save",
+            help = "Path to save location of merged labels - can be the same as --labels_in to overwrite", 
+            default = None, type = str, required=True)
+    
+    parser.add_argument("--merge", dest = "merge_file",
+            help = "Path to merge yaml file", 
+            default = None, type = str, required=False)
+    
+    return parser.parse_args()
+
+
+def main():
+    args = arg_parse()
+    use_case = input("Do you need to generate a merger file (G) or merge classes from existing class_merger.yaml (M)? G/M?")
+    # generating a merger file
+    if use_case.lower() == "g":
+        # check for new class list and data.yaml of classes
+        if args.new_cls is None or args.data is None or args.save is None:
+            sys.exit("Please ensure --newclasses, --data and --save arguments are included")
+        if args.labels_in is not None or args.merge_file is not None:
+            print("WARNING: --labels_in and --merge are being ignored\n") 
+
+        labels_in = None
+        save = args.save
+        merge_file = None
+        data = args.data
+        new_cls = args.new_cls
+    
+    # merge from exisitng class_merger.yaml
+    elif use_case.lower() == "m":
+        if args.labels_in is None or args.save is None or args.merge_file is None:
+            sys.exit("Please ensure --labels_in, --save and --merge arguments are included")
+        if args.new_cls is not None or args.data is not None:
+            print("WARNING: --newclasses and --data arguments are being ignored\n") 
+
+        labels_in = args.labels_in
+        save = args.save
+        merge_file = args.merge_file
+        data = None
+        new_cls = None
+    
+    else:
+        sys.exit("Please type G to generate a class_merger.yaml file or M to merge labels from an existing class_merger.yaml file")
+
+    mc = mergeClasses(labels_in, save, merge_file, data, new_cls, use_case)
+    mc.run()
+    print("\nDONE")
+if __name__=='__main__':
+    main()
